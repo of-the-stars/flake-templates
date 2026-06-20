246ca49cc971e42ea83b3bacdae1c2bd480d4977
of-the-stars <stellae.incaelis@gmail.com>
2026-06-20 12:23:37 -0500

    Removed flake-utils dependency and rewrote it myself

--- rust/flake.nix
+++ rust/flake.nix
@@ -4,10 +4,8 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     crane = {
       url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
@@ -19,76 +17,107 @@
     {
       self,
       nixpkgs,
-      flake-utils,
       crane,
       rust-overlay,
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
+    let
+      systems = [
+        "x86_64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+      pkgsFor =
+        system:
+        import nixpkgs {
           inherit system;
           overlays = [ (import rust-overlay) ];
         };
+      packageForSystem =
+        system:
+        let
+          pkgs = (pkgsFor system);
 
-        # Builds the rust components from the toolchain file, or defaults back to the latest nightly build
-        rust-toolchain =
-          if builtins.pathExists ./rust-toolchain.toml then
-            pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml
-          else
-            pkgs.rust-bin.stable.latest.default.override {
-              extensions = [ "rust-src" ];
-            };
+          # Builds the rust components from the toolchain file, or defaults back to the latest nightly build
+          rust-toolchain =
+            if builtins.pathExists ./rust-toolchain.toml then
+              pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml
+            else
+              pkgs.rust-bin.stable.latest.default.override {
+                extensions = [ "rust-src" ];
+              };
 
-        # Instantiates custom craneLib using toolchain
-        craneLib = (crane.mkLib pkgs).overrideToolchain rust-toolchain;
+          # Instantiates custom craneLib using toolchain
+          craneLib = (crane.mkLib pkgs).overrideToolchain rust-toolchain;
 
-        src = craneLib.cleanCargoSource ./.;
-        pname = craneLib.crateNameFromCargoToml { cargoToml = ./Cargo.toml; }.pname;
+          src = craneLib.cleanCargoSource ./.;
+          pname = craneLib.crateNameFromCargoToml { cargoToml = ./Cargo.toml; }.pname;
 
-        # Common arguments shared between buildPackage and buildDepsOnly
-        commonArgs = {
-          inherit src;
-          strictDeps = true;
+          # Common arguments shared between buildPackage and buildDepsOnly
+          commonArgs = {
+            inherit src;
+            strictDeps = true;
 
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-          ];
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+            ];
 
-          buildInputs = with pkgs; [
-            openssl
-          ];
+            buildInputs = with pkgs; [
+              openssl
+            ];
+          };
+
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+          crane-package = craneLib.buildPackage (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+            }
+          );
+        in
+        {
+          default = crane-package;
         };
+      devShellForSystem =
+        system:
+        let
+          pkgs = pkgsFor system;
 
-        cargoArtifacts = craneLib.buildDepsOnly (commonArgs);
+          # Builds the rust components from the toolchain file, or defaults back to the latest nightly build
+          rust-toolchain =
+            if builtins.pathExists ./rust-toolchain.toml then
+              pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml
+            else
+              pkgs.rust-bin.stable.latest.default.override {
+                extensions = [ "rust-src" ];
+              };
+        in
+        {
+          default = pkgs.mkShell {
+            # Inherits buildInputs from crane-package
+            inputsFrom = [ (packageForSystem system) ];
 
-        crane-package = craneLib.buildPackage (
-          commonArgs
-          // {
-            inherit cargoArtifacts;
-          }
-        );
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          # Inherits buildInputs from crane-package
-          inputsFrom = [ crane-package ];
+            # Additional packages for the dev environment
+            packages = with pkgs; [
+            ];
 
-          # Additional packages for the dev environment
-          packages = with pkgs; [
-          ];
+            shellHook = "";
 
-          shellHook = "";
-
-          env = {
-            # Needed for rust-analyzer
-            RUST_SRC_PATH = "${rust-toolchain}/lib/rustlib/src/rust/library";
+            env = {
+              # Needed for rust-analyzer
+              RUST_SRC_PATH = "${rust-toolchain}/lib/rustlib/src/rust/library";
+            };
           };
         };
-
-        packages.default = crane-package;
-
-        formatter = pkgs.nixfmt-tree;
-      }
-    );
+      formatterForSystem =
+        system:
+        let
+          pkgs = pkgsFor system;
+        in
+        pkgs.nixfmt-tree;
+    in
+    {
+      # packages = forAllSystems (system: packageForSystem system);
+      # devShells = forAllSystems (system: devShellForSystem system);
+      # formatter = forAllSystems (system: formatterForSystem system);
+    };
 }
